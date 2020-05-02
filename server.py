d0c1657a883e93e0a581bfddde19c2dbe9c89edd
Siddhant Gupta <siddhant.gupta021295@gmail.com>
2020-05-02 22:41:20 +0530

    file upload working

---
+++ server.py
@@ -0,0 +1,48 @@
+import os
+from flask import Flask, flash, request, redirect, url_for,render_template
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './files'
+ALLOWED_EXTENSIONS = {'csv','xlsx'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+
+
+@app.route('/login')
+def hello_world():
+    return 'login page'
+
+@app.route('/uploaded_file')
+def upload_doc():
+    return 'file uploaded'
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return 'file uploaded'
+    else:
+        return render_template('upload.html')
+
+        
+
+    
